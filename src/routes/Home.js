cc775e5a033df4b63bbb9ad7592a8d145a9135cf
yyb <yyb0918@gmail.com>
2025-07-22 17:03:13 +0900

    commit gogogo

---
+++ src/routes/Home.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Movie from "../components/Movie";
+
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const res = await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimun_rating=8.5&sort_by=year`
+    );
+    const json = await res.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  console.log(movies);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie, idx) => (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Home;
